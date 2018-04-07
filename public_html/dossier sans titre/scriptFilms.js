92dd6b4e537865c0125aed5fdae51bd91fef92f1
Alain doccitanie <al1.34.ae@gmail.com>
2018-04-07 07:28:56 +0200

    Initial commit
    student exercice
    le but etant de manipuler le dom en JS Jquerry et peut etre un jour Angular

---
+++ public_html/dossier sans titre/scriptFilms.js
@@ -0,0 +1,43 @@
+
+{ 
+  "DisneyCollections": "Présente:", 
+  "DeVeaDus Disney": [ 
+      {
+          "titre": "Bambi", 
+          "producteur":"Walt Disney",
+          "Année":"1951",
+          "Image":"src='../images/bambi.jpg'"
+      }, 
+      {
+          "titre": "Dory", 
+          "producteur": "Walt Disney",
+           "Année":"1951",
+          "Image":"src='../images/dory.jpg'"
+      }, 
+      {
+          "titre": "pinocchio", 
+          "producteur": "Walt Disney",
+           "Année":"1951",
+          "Image":"src='../images/pinocchio.jpg'"
+      },
+       {
+          "titre": "Le Roi Lion", 
+          "producteur":"Walt Disney",
+          "Année":"1951",
+          "Image":"src='../images/roi_Lion.jpg'"
+      }, 
+      {
+          "titre": "Aristochat", 
+          "producteur": "Walt Disney",
+           "Année":"1951",
+          "Image":"src='../images/aristochat.jpg'"
+      }, 
+      {
+          "titre": "Peter Pan", 
+          "producteur": "Walt Disney",
+           "Année":"1951",
+          "Image":"src='../images/peter_Pan.jpg'"
+      }
+   ] 
+} 
+DisneyCollections().document.getElementById("WD").innerHTML
